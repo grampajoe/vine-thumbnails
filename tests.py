61a3cba3c8a90f31febb4240cd6907391a944e00
Joe Friedl <joe.friedl@offerpop.com>
2013-08-08 16:05:10 -0400

    Simplify the tests

--- tests.py
+++ tests.py
@@ -5,24 +5,15 @@ from app import get_vine_id
 
 class TestVineID(unittest.TestCase):
     """Tests for Vine ID utils."""
-    def test_get_id_from_url(self):
-        """Test getting a Vine ID from a URL"""
-        url = 'https://vine.co/v/hhmEEAIAzT2'
-
-        vine_id = get_vine_id(url)
-
+    def test_get_vine_id(self):
+        """Test getting a Vine ID"""
+        # From a URL
+        vine_id = get_vine_id('https://vine.co/v/hhmEEAIAzT2')
         self.assertEqual(vine_id, 'hhmEEAIAzT2')
 
-    def test_get_id_from_id(self):
-        """Test getting an ID from an ID"""
-        original_id = 'hhmEEAIAzT2'
+        # From an ID
+        vine_id = get_vine_id('hhmEEAIAzT2')
+        self.assertEqual(vine_id, 'hhmEEAIAzT2')
 
-        vine_id = get_vine_id(original_id)
-
-        self.assertEqual(vine_id, original_id)
-
-    def test_get_id_from_bad_string(self):
-        """Test failing to get a Vine ID"""
-        bad_string = 'w?F@F#JFIJF'
-
-        self.assertRaises(ValueError, get_vine_id, bad_string)
+        # From a bad string
+        self.assertRaises(ValueError, get_vine_id, 'w?F@F#JFIJF')
