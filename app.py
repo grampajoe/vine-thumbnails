17ba8d5eb5832c22eb72ad88dd6316c730e86177
Joe Friedl <joe@joefriedl.net>
2014-05-03 16:02:25 -0400

    Cache thumbnails

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from StringIO import StringIO
+import os
 import re
+from StringIO import StringIO
 
 from flask import (
     Flask, make_response, request, render_template, redirect, url_for)
+from flask.ext.cache import Cache
 import requests
 from bs4 import BeautifulSoup
 import newrelic.agent
@@ -10,6 +12,14 @@ from PIL import Image
 
 
 app = Flask(__name__)
+
+
+cache = Cache(app, config={
+    'CACHE_TYPE': os.environ.get('CACHE_TYPE', 'null'),
+    'CACHE_MEMCACHED_SERVERS': os.environ.get('MEMCACHED_CLOUD_SERVERS', '').split(','),
+    'CACHE_MEMCACHED_USERNAME': os.environ.get('MEMCACHED_CLOUD_USERNAME'),
+    'CACHE_MEMCACHED_PASSWORD': os.environ.get('MEMCACHED_CLOUD_PASSWORD'),
+})
 
 
 VINE_ID_PATTERN = re.compile(r'^\w+$')
@@ -66,19 +76,12 @@ def get_thumb_url(vine):
     return parsed.find('meta', itemprop='thumbnailUrl').attrs['content']
 
 
-@app.route('/t/<vine_id>/')
-def vine_thumb(vine_id):
-    """Get a Vine video thumbnail.
+@cache.memoize()
+def make_thumb(vine_id, size):
+    """Make a thumbnail and stuff."""
+    app.logger.debug('Making %r thumb for %s.' % (size, vine_id))
 
-    Add ?s=SIZE to the URL to specify the maximum width of the thumbnail.
-    Vine "poster" images are 480x480.
-    """
     vine = get_vine(vine_id)
-
-    try:
-        size = (int(request.args.get('s', 480)), 480)
-    except ValueError:
-        size = (480, 480)
 
     thumb_url = get_thumb_url(vine)
 
@@ -98,7 +101,29 @@ def vine_thumb(vine_id):
     saved_thumb = StringIO()
     thumb.save(saved_thumb, 'JPEG')
 
-    response = make_response(saved_thumb.getvalue())
+    return saved_thumb.getvalue()
+
+
+@app.route('/t/<vine_id>/')
+def vine_thumb(vine_id):
+    """Get a Vine video thumbnail.
+
+    Add ?s=SIZE to the URL to specify the maximum width of the thumbnail.
+    Vine "poster" images are 480x480.
+    """
+    try:
+        size = (int(request.args.get('s', 480)), 480)
+    except ValueError:
+        size = (480, 480)
+
+    content = make_thumb(vine_id, size)
+
+    newrelic.agent.add_custom_parameter(
+        'thumb_file_size',
+        len(content),
+    )
+
+    response = make_response(content)
     response.headers['Content-Type'] = 'image/jpeg'
 
     return response, 200
